635900d8d41887227563998602660aa6aa799dda
Joa Leitola <leitola.joa@gmail.com>
2021-07-29 15:56:08 +0300

    add intro section, needs later fixing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavigationBar from "./Components/NavigationBar/NavigationBar";
+import Intro from "./Sections/Intro/Intro";
 
 const App = () => {
     return (
         <div className="App">
             <NavigationBar />
+            <Intro />
         </div>
     );
 };
---
+++ src/Sections/Intro/Intro.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Intro.css";
+import RequestInviteButton from "../../Components/RequestInviteButton/RequestInviteButton";
+import mockups from "../../images/image-mockups.png";
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <div className="info">
+                <h1 className="header">
+                    Next generation <br /> digital banking
+                </h1>
+                <p className="info-p">
+                    Take your financial life online. Your Easybank account{" "}
+                    <br /> will be a one-stop-shop for spending, saving, <br />{" "}
+                    budgeting, investing, and much more
+                </p>
+                <RequestInviteButton />
+            </div>
+            <div className="picture">
+                <img src={mockups} alt="4-phones" />
+            </div>
+        </div>
+    );
+};
+
+export default Intro;
